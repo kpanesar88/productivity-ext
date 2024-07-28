87cd495d1b69cb722c9df883699aa0f8fe9139f2
Heitan <heitan104@gmail.com>
2024-07-28 00:54:49 -0400

    changes to app.py

--- ProductivityExtension/Backend/app/app.py
+++ ProductivityExtension/Backend/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, jsonify
 
 
 app = Flask(__name__)
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
   return render_template("index.html")
+
+
+@app.route("/data")
+def get_data():
+  data = {
+    "seconds": 0
+  }
+  
+  return jsonify(data)
